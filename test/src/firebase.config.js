734a16ef3a5615c484a11b06b8e2f270128b1646
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-03-28 18:15:10 +0800

    firebaseChanged

--- test/src/firebase.config.js
+++ test/src/firebase.config.js
@@ -1,14 +1,29 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyACRDCmr-kjyW6S9Ry63muj7jUCZt4kl0U",
+//   authDomain: "testop-bb310.firebaseapp.com",
+//   projectId: "testop-bb310",
+//   storageBucket: "testop-bb310.appspot.com",
+//   messagingSenderId: "1047476759466",
+//   appId: "1:1047476759466:web:16fed376cea23ba36e19d3",
+//   measurementId: "G-2HH8E982XS",
+// };
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACRDCmr-kjyW6S9Ry63muj7jUCZt4kl0U",
-  authDomain: "testop-bb310.firebaseapp.com",
-  projectId: "testop-bb310",
-  storageBucket: "testop-bb310.appspot.com",
-  messagingSenderId: "1047476759466",
-  appId: "1:1047476759466:web:16fed376cea23ba36e19d3",
-  measurementId: "G-2HH8E982XS",
+  apiKey: "AIzaSyDvCezuxmquMMrxViVGYTQLVJhipQktU-4",
+  authDomain: "testotp1-1170a.firebaseapp.com",
+  projectId: "testotp1-1170a",
+  storageBucket: "testotp1-1170a.appspot.com",
+  messagingSenderId: "626076317739",
+  appId: "1:626076317739:web:5f3d74917c1e7f4a02126e",
+  measurementId: "G-04BPP8Q03G",
 };
 
 const app = initializeApp(firebaseConfig);
